e53fadd44babbda437c9422843a46857182c6010
AntoinePerrin25 <antoine.perrin1@utbm.fr>
2025-04-15 18:25:05 +0200

    Add build script for CSV Search GUI application and enhance nob.c for raylib integration

--- nob.c
+++ nob.c
@@ -24,25 +24,25 @@ int main(int argc, char **argv)
 
     bool *opti = flag_bool("O", false, "Enable O2 optimizations");
     
-    bool *pedantic = flag_bool("pedantic", false, "Enable pedantic flag");
-    flag_add_alias(pedantic, "ped");
+    bool *pedantic = flag_bool("ped", false, "Enable pedantic flag");
+    flag_add_alias(pedantic, "pedantic");
     
-    bool *unused = flag_bool("unused", false, "Disable unused warnings");
-    flag_add_alias(unused, "u");
+    bool *unused = flag_bool("u", false, "Disable unused warnings");
+    flag_add_alias(unused, "unused");
     
     bool *all = flag_bool("a", true, "Enable all common flags");
     flag_add_alias(all, "all");
-    flag_add_alias(debug, "a");
     flag_add_alias(debug, "all");
-    flag_add_alias(opti, "a");
     flag_add_alias(opti, "all");
-    flag_add_alias(pedantic, "a");
     flag_add_alias(pedantic, "all");
-    flag_add_alias(unused, "a");
     flag_add_alias(unused, "all");
 
     bool *run = flag_bool("r", false, "Run the command after building it");
     flag_add_alias(run, "run");
+    
+    bool *gui = flag_bool("gui", false, "Build the GUI version with raylib");
+    
+    bool *download_raylib = flag_bool("dl-raylib", false, "Download and build raylib");
 
     if (!flag_parse(argc, argv))
     {
@@ -58,30 +58,99 @@ int main(int argc, char **argv)
         }
         else
         {
-            Nob_Cmd cmd = {0};
-            nob_cmd_append(&cmd, "gcc", "-Wall", "-Wextra");
-
-            if (*debug)
-                nob_cmd_append(&cmd, "-g");
-            if (*opti)
-                nob_cmd_append(&cmd, "-O2");
-            if (*pedantic)
-                nob_cmd_append(&cmd, "-pedantic", "-std=c99");
-            if (*unused)
-                nob_cmd_append(&cmd, "-Wno-unused-parameter");
+            // Check if we need to download raylib
+            if (*download_raylib) {
+                printf("Downloading and building raylib...\n");
                 
-            nob_cmd_append(&cmd, "-Iinclude");
-            nob_cmd_append(&cmd, "-Llib");
-            nob_cmd_append(&cmd, "-o", "search");
-            nob_cmd_append(&cmd, "src/main.c", "src/csv.c");
-            if (!nob_cmd_run_sync(cmd)) return 1;
-            else
-            {
-                if (*run)
+                // Create lib directory if it doesn't exist
+                Nob_Cmd mkdir_cmd = {0};
+                #ifdef _WIN32
+                nob_cmd_append(&mkdir_cmd, "mkdir", "lib");
+                #else
+                nob_cmd_append(&mkdir_cmd, "mkdir", "-p", "lib");
+                #endif
+                nob_cmd_run_sync(mkdir_cmd);
+                
+                // Clone raylib repository
+                Nob_Cmd git_cmd = {0};
+                nob_cmd_append(&git_cmd, "git", "clone", "https://github.com/raysan5/raylib.git", "lib/raylib");
+                if (!nob_cmd_run_sync(git_cmd)) {
+                    fprintf(stderr, "Failed to clone raylib repository\n");
+                    return 1;
+                }
+                
+                // Build raylib
+                Nob_Cmd build_cmd = {0};
+                #ifdef _WIN32
+                // On Windows, we'll use MinGW's make or CMake
+                nob_cmd_append(&build_cmd, "cd", "lib\\raylib\\src", "&&", "mingw32-make", "PLATFORM=PLATFORM_DESKTOP");
+                #else
+                nob_cmd_append(&build_cmd, "cd", "lib/raylib/src", "&&", "make", "PLATFORM=PLATFORM_DESKTOP");
+                #endif
+                if (!nob_cmd_run_sync(build_cmd)) {
+                    fprintf(stderr, "Failed to build raylib\n");
+                    return 1;
+                }
+                
+                printf("Raylib downloaded and built successfully\n");
+            }
+            
+            if (*gui) {
+                // Build the GUI version with raylib
+                Nob_Cmd cmd = {0};
+                nob_cmd_append(&cmd, "gcc", "-Wall", "-Wextra");
+                
+                if (*debug)
+                    nob_cmd_append(&cmd, "-g");
+                if (*opti)
+                    nob_cmd_append(&cmd, "-O2");
+                if (*pedantic)
+                    nob_cmd_append(&cmd, "-pedantic", "-std=c99");
+                if (*unused)
+                    nob_cmd_append(&cmd, "-Wno-unused-parameter");
+                    
+                nob_cmd_append(&cmd, "-Iinclude");
+                
+                nob_cmd_append(&cmd, "-Llib/raylib/src");
+                nob_cmd_append(&cmd, "-o", "gui_search.exe");
+                nob_cmd_append(&cmd, "src/csv.c", "src/gui/gui_search.c");
+                nob_cmd_append(&cmd, "-lraylib", "-lopengl32", "-lgdi32", "-lwinmm", "-lws2_32");
+                
+                if (!nob_cmd_run_sync(cmd)) return 1;
+                else {
+                    if (*run) {
+                        cmd = (Nob_Cmd){0};
+                        nob_cmd_append(&cmd, "./gui_search");
+                        if (!nob_cmd_run_sync(cmd)) return 1;
+                    }
+                }
+            } else {
+                // Build the regular command-line version
+                Nob_Cmd cmd = {0};
+                nob_cmd_append(&cmd, "gcc", "-Wall", "-Wextra");
+
+                if (*debug)
+                    nob_cmd_append(&cmd, "-g");
+                if (*opti)
+                    nob_cmd_append(&cmd, "-O2");
+                if (*pedantic)
+                    nob_cmd_append(&cmd, "-pedantic", "-std=c99");
+                if (*unused)
+                    nob_cmd_append(&cmd, "-Wno-unused-parameter");
+                    
+                nob_cmd_append(&cmd, "-Iinclude");
+                nob_cmd_append(&cmd, "-Llib");
+                nob_cmd_append(&cmd, "-o", "search");
+                nob_cmd_append(&cmd, "src/main.c", "src/csv.c");
+                if (!nob_cmd_run_sync(cmd)) return 1;
+                else
                 {
-                    cmd = (Nob_Cmd){0};
-                    nob_cmd_append(&cmd, "main");
-                    if (!nob_cmd_run_sync(cmd)) return 1;
+                    if (*run)
+                    {
+                        cmd = (Nob_Cmd){0};
+                        nob_cmd_append(&cmd, "./search");
+                        if (!nob_cmd_run_sync(cmd)) return 1;
+                    }
                 }
             }
         }
