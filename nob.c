d9b80d1e7c144043eeae7f3db9b5465ba4571861
AntoinePerrin25 <antoine.perrin1@utbm.fr>
2025-04-21 12:36:37 +0200

    Update nob flags aliases and removed unused suppression flag

--- nob.c
+++ nob.c
@@ -16,34 +16,12 @@ int main(int argc, char **argv)
 {
     NOB_GO_REBUILD_URSELF(argc, argv);
 
-    bool *help = flag_bool("h", false, "Help Flag to display Usage");
-    flag_add_alias(help, "help");
-
-    bool *debug = flag_bool("g", false, "Enable debug information");
-    flag_add_alias(debug, "debug");
-
-    bool *opti = flag_bool("O", false, "Enable O2 optimizations");
+    bool *help = flag_bool_aliases("h", false, "Help Flag to display Usage", "help");
+    bool *debug = flag_bool_aliases("g", false, "Enable debug information", "debug");
+    bool *opti = flag_bool_aliases("O", false, "Enable O2 optimizations", "opti");
+    bool *pedantic = flag_bool_aliases("pedantic", false, "Enable pedantic flag", "ped");
+    bool *all = flag_bool_aliases("a", true, "Enable all common flags", "all");
     
-    bool *pedantic = flag_bool("pedantic", false, "Enable pedantic flag");
-    flag_add_alias(pedantic, "ped");
-    
-    bool *unused = flag_bool("unused", false, "Disable unused warnings");
-    flag_add_alias(unused, "u");
-    
-    bool *all = flag_bool("a", true, "Enable all common flags");
-    flag_add_alias(all, "all");
-    flag_add_alias(debug, "a");
-    flag_add_alias(debug, "all");
-    flag_add_alias(opti, "a");
-    flag_add_alias(opti, "all");
-    flag_add_alias(pedantic, "a");
-    flag_add_alias(pedantic, "all");
-    flag_add_alias(unused, "a");
-    flag_add_alias(unused, "all");
-
-    bool *run = flag_bool("r", false, "Run the command after building it");
-    flag_add_alias(run, "run");
-
     if (!flag_parse(argc, argv))
     {
         printf("Error parsing flags\n");
@@ -61,29 +39,19 @@ int main(int argc, char **argv)
             Nob_Cmd cmd = {0};
             nob_cmd_append(&cmd, "gcc", "-Wall", "-Wextra");
 
-            if (*debug)
+
+            if (*all || *debug)
                 nob_cmd_append(&cmd, "-g");
-            if (*opti)
+            if (*all || *opti)
                 nob_cmd_append(&cmd, "-O2");
-            if (*pedantic)
+            if (*all || *pedantic)
                 nob_cmd_append(&cmd, "-pedantic", "-std=c99");
-            if (*unused)
-                nob_cmd_append(&cmd, "-Wno-unused-parameter");
                 
             nob_cmd_append(&cmd, "-Iinclude");
             nob_cmd_append(&cmd, "-Llib");
             nob_cmd_append(&cmd, "-o", "search");
             nob_cmd_append(&cmd, "src/main.c", "src/csv.c");
             if (!nob_cmd_run_sync(cmd)) return 1;
-            else
-            {
-                if (*run)
-                {
-                    cmd = (Nob_Cmd){0};
-                    nob_cmd_append(&cmd, "main");
-                    if (!nob_cmd_run_sync(cmd)) return 1;
-                }
-            }
         }
     }
     return 0;
