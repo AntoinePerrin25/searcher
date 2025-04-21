49b76cc19bfe8be76672b1bc25567189bfdee52a
AntoinePerrin25 <antoine.perrin1@utbm.fr>
2025-04-21 14:46:32 +0200

    Added static nob.h file and added headers to NOB_GO_REBUILD_URSELF_PLUS

--- nob.c
+++ nob.c
@@ -1,5 +1,5 @@
 #define NOB_IMPLEMENTATION
-#include <nob.h>
+#include "include/nob.h"
 //
 #define FLAG_IMPLEMENTATION
 #include "include/flag.h"
@@ -14,13 +14,14 @@ void usage(FILE* stream)
 
 int main(int argc, char **argv)
 {
-    NOB_GO_REBUILD_URSELF(argc, argv);
+    NOB_GO_REBUILD_URSELF_PLUS(argc, argv, "include/flag.h", "include/nob.h");
 
     bool *help = flag_bool_aliases("h", false, "Help Flag to display Usage", "help");
     bool *debug = flag_bool_aliases("g", false, "Enable debug information", "debug");
     bool *opti = flag_bool_aliases("O", false, "Enable O2 optimizations", "opti");
     bool *pedantic = flag_bool_aliases("pedantic", false, "Enable pedantic flag", "ped");
-    bool *all = flag_bool_aliases("a", true, "Enable all common flags", "all");
+    bool *openMP = flag_bool_aliases("openmp", false, "Enable OpenMP support", "omp");
+    bool *all = flag_bool_aliases("a", false, "Enable all common flags", "all");
     
     if (!flag_parse(argc, argv))
     {
@@ -46,10 +47,10 @@ int main(int argc, char **argv)
                 nob_cmd_append(&cmd, "-O2");
             if (*all || *pedantic)
                 nob_cmd_append(&cmd, "-pedantic", "-std=c99");
-             
-            nob_cmd_append(&cmd, "-fopenmp");
+            if (*all || *openMP)
+                nob_cmd_append(&cmd, "-fopenmp");
+            
             nob_cmd_append(&cmd, "-Iinclude");
-            nob_cmd_append(&cmd, "-Llib");
             nob_cmd_append(&cmd, "-o", "search");
             nob_cmd_append(&cmd, "src/main.c", "src/csv.c");
             if (!nob_cmd_run_sync(cmd)) return 1;
